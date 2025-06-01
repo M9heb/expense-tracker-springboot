31291d3c0435e63269263c20ad95115240a1f0d4
M9heb <123528241+M9heb@users.noreply.github.com>
2025-06-01 11:11:43 +0430

    Completes the remaining services

--- src/main/java/com/example/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/example/expensetracker/controller/ExpenseController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.expensetracker.model.Expense;
 import com.example.expensetracker.service.ExpenseService;
@@ -23,6 +26,43 @@ public class ExpenseController {
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
         model.addAttribute("expenses", expenses);
         model.addAttribute("totalAmount", totalAmount);
-        return "index"; // This will return the index.html template
+        return "index";
+    }
+
+    @GetMapping("/addExpense")
+    public String showAddExpensePage(Model model) {
+        Expense expense = new Expense();
+        model.addAttribute("expense", expense);
+        return "add-expense";
+    }
+
+    @PostMapping("/saveExpense")
+    public String saveExpense(@ModelAttribute("expense") Expense expense, Model model) {
+
+        expenseService.saveExpense(expense);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editExpense/{id}")
+    public String showUpdateExpensePage(@PathVariable("id") Long id, Model model) {
+        Expense expense = expenseService.getExpenseById(id);
+        model.addAttribute("expense", expense);
+        return "edit_expense";
+    }
+
+    @PostMapping("/updateExpense/{id}")
+    public String updateExpense(@PathVariable("id") long id, @ModelAttribute("expense") Expense expense) {
+        Expense existingExpense = expenseService.getExpenseById(id);
+        // existingExpense.setId(id);
+        existingExpense.setDescription(expense.getDescription());
+        existingExpense.setAmount(expense.getAmount());
+        expenseService.saveExpense(existingExpense);
+        return "redirect:/";
+    }
+
+    @GetMapping("/deleteExpense/{id}")
+    public String deleteExpense(@PathVariable("id") long id) {
+        expenseService.deleteExpenseById(id);
+        return "redirect:/";
     }
 }
